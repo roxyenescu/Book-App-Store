4fbd28c7ba7c9fd8576ee3f98a570907b392940a
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2025-04-29 23:03:58 +0300

    Frontend: SignUp page + LogIn page

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,12 +1,61 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LogIn = () => {
   return (
-    <div>
-      LogIn
+    <div className='h-screen bg-zinc-900 px-12 py-8 flex items-center justify-center'>
+      <div className='bg-zinc-800 rounded-lg px-8 py-5 w-full md:w-3/6 lg:w-2/6'>
+        <p className='text-zinc-200 text-2xl font-semibold'>
+          LOG IN
+        </p>
+
+        <div className='mt-4'>
+          <div>
+            <label htmlFor='' className='text-zinc-400'>
+              Username
+            </label>
+            <input
+              type='text'
+              className='w-full mt-2 bg-zinc-900 text-zinc-100 p-2 outline-none'
+              placeholder='ex: maria_popescu_20'
+              name='username'
+              required
+            />
+          </div>
+
+          <div className='mt-4'>
+            <label htmlFor='' className='text-zinc-400'>
+              Password
+            </label>
+            <input
+              type='password'
+              className='w-full mt-2 bg-zinc-900 text-zinc-100 p-2 outline-none'
+              placeholder='********'
+              name='password'
+              required
+            />
+          </div>
+
+          <div className='mt-4'>
+            <button className='w-full bg-blue-500 text-white font-semibold py-2 rounded hover:bg-blue-200'>
+              Log In
+            </button>
+          </div>
+
+          <p className='flex mt-4 items-center justify-center text-zinc-200 font-semibold'>
+            OR
+          </p>
+
+          <p className='flex mt-4 items-center justify-center text-zinc-500 font-semibold'>
+            Don't have an account? &nbsp;
+            <Link to='/sign-up' className='text-blue-200 hover:text-blue-500'>
+              <u>Sing Up</u>
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default LogIn
-
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,113 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   return (
-    <div>
-      SignUp
+    <div className='h-auto bg-zinc-900 px-12 py-8 flex items-center justify-center'>
+      <div className='bg-zinc-800 rounded-lg px-8 py-5 w-full md:w-3/6 lg:w-2/6'>
+        <p className='text-zinc-200 text-2xl font-semibold'>
+          SIGN UP
+        </p>
+
+        <div className='mt-4'>
+          <div>
+            <label htmlFor='' className='text-zinc-400'>
+              Username
+            </label>
+            <input
+              type='text'
+              className='w-full mt-2 bg-zinc-900 text-zinc-100 p-2 outline-none'
+              placeholder='ex: maria_popescu_20'
+              name='username'
+              required
+            />
+          </div>
+
+          <div className='mt-4'>
+            <label htmlFor='' className='text-zinc-400'>
+              First name
+            </label>
+            <input
+              type='text'
+              className='w-full mt-2 bg-zinc-900 text-zinc-100 p-2 outline-none'
+              placeholder='ex: Maria'
+              name='firstname'
+              required
+            />
+          </div>
+
+          <div className='mt-4'>
+            <label htmlFor='' className='text-zinc-400'>
+              Last name
+            </label>
+            <input
+              type='text'
+              className='w-full mt-2 bg-zinc-900 text-zinc-100 p-2 outline-none'
+              placeholder='ex: Popescu'
+              name='lastname'
+              required
+            />
+          </div>
+
+          <div className='mt-4'>
+            <label htmlFor='' className='text-zinc-400'>
+              Email
+            </label>
+            <input
+              type='text'
+              className='w-full mt-2 bg-zinc-900 text-zinc-100 p-2 outline-none'
+              placeholder='email@example.com'
+              name='email'
+              required
+            />
+          </div>
+
+          <div className='mt-4'>
+            <label htmlFor='' className='text-zinc-400'>
+              Password
+            </label>
+            <input
+              type='password'
+              className='w-full mt-2 bg-zinc-900 text-zinc-100 p-2 outline-none'
+              placeholder='********'
+              name='password'
+              required
+            />
+          </div>
+
+          <div className='mt-4'>
+            <label htmlFor='' className='text-zinc-400'>
+              Addresss
+            </label>
+            <textarea
+              rows='5'
+              className='w-full mt-2 bg-zinc-900 text-zinc-100 p-2 outline-none'
+              placeholder='address'
+              name='address'
+              required
+            />
+          </div>
+
+          <div className='mt-4'>
+            <button className='w-full bg-blue-500 text-white font-semibold py-2 rounded hover:bg-blue-200'>
+              Sign Up
+            </button>
+          </div>
+
+          <p className='flex mt-4 items-center justify-center text-zinc-200 font-semibold'>
+            OR
+          </p>
+
+          <p className='flex mt-4 items-center justify-center text-zinc-500 font-semibold'>
+            Already have an account? &nbsp;
+            <Link to='/log-in' className='text-blue-200 hover:text-blue-500'>
+              <u>Log In</u>
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SignUp
