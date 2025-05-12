fc98edf0663e06bdb105d2f8776a6dcd9181146e
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2025-05-12 23:18:26 +0300

    Frontend: Profile page + Sidebar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions } from './store/auth';
 import Home from './pages/Home';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-import { Routes, Route } from 'react-router-dom';
 import AllBooks from './pages/AllBooks';
 import LogIn from './pages/LogIn';
 import SignUp from './pages/SignUp';
 import Cart from './pages/Cart';
 import Profile from './pages/Profile';
 import ViewBookDetails from './components/ViewBookDetails/ViewBookDetails';
-import { useDispatch, useSelector } from 'react-redux';
-import { authActions } from './store/auth';
+import Favourites from './components/Profile/Favourites';
+import UserOrderHistory from './components/Profile/UserOrderHistory';
+import Settings from './components/Profile/Settings';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -24,19 +27,23 @@ const App = () => {
 
   return (
     <div>
-        <Navbar />
+      <Navbar />
 
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/all-books" element={<AllBooks />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/log-in" element={<LogIn />} />
-          <Route path="/view-book-details/:id" element={<ViewBookDetails />} />
-        </Routes>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/all-books" element={<AllBooks />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<Profile />} >
+          <Route index element={<Favourites />} />
+          <Route path="/profile/order-history" element={<UserOrderHistory />} />
+          <Route path="/profile/settings" element={<Settings />} />
+        </Route>
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/log-in" element={<LogIn />} />
+        <Route path="/view-book-details/:id" element={<ViewBookDetails />} />
+      </Routes>
 
-        <Footer />
+      <Footer />
     </div>
   );
 };
