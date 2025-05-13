97b4eed9fd22208ea6306fadeda668399bd77d9e
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2025-05-13 23:58:52 +0300

    Frontend: Add to Favourites, Add to Cart, Edit Book, Delete Book buttons + Responsive ViewBookDetails page

--- client/src/components/ViewBookDetails/ViewBookDetails.jsx
+++ client/src/components/ViewBookDetails/ViewBookDetails.jsx
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Loader from '../Loader/Loader';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { GrLanguage } from "react-icons/gr";
+import { FaHeart } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
 
 const ViewBookDetails = () => {
     const { id } = useParams();
     const [Data, setData] = useState();
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+    const role = useSelector((state) => state.auth.role);
+
     useEffect(() => {
         const fetch = async () => {
             const response = await axios.get(
@@ -20,11 +28,42 @@ const ViewBookDetails = () => {
     return (
         <>
             {Data && (
-                <div className='px-4 md:px-12 py-8 bg-zinc-900 flex flex-col md:flex-row gap-8'>
-                    <div className='bg-zinc-800 rounded p-4 h-[60vh] lg:h-[88vh] w-full lg:w-3/6 flex items-center justify-center'>
-                        {" "}
-                        <img src={Data.url} alt='/' className='h-[50vh] lg:h-[70vh] rounded' />
+                <div className='px-4 md:px-12 py-8 bg-zinc-900 flex flex-col lg:flex-row gap-8 items-start'>
+                    <div className='w-full lg:w-3/6'>
+                        <div className='flex flex-col lg:flex-row justify-around bg-zinc-800 p-12 rounded'>
+                            <img src={Data.url} alt='/' className='h-[50vh] md:h-[60vh] lg:h-[70vh] rounded' />
+                            {
+                                isLoggedIn === true && role === "user" && (
+                                    <div className='flex flex-row lg:flex-col items-center justify-between lg:justify-start mt-4 lg:mt-0'>
+                                        <button className='bg-white rounded lg:rounded-full text-3xl p-3 text-red-500 flex items-center justify-center'>
+                                            <FaHeart />
+                                            <span className='ms-4 block lg:hidden'>Add to Favourites</span>
+                                        </button>
+                                        <button className='text-white rounded lg:rounded-full text-3xl p-3 mt-0 lg:mt-8 bg-blue-500 flex items-center justify-center'>
+                                            <FaShoppingCart />
+                                            <span className='ms-4 block lg:hidden'>Add to Cart</span>
+                                        </button>
+                                    </div>
+                                )
+                            }
+
+                            {
+                                isLoggedIn === true && role === "admin" && (
+                                    <div className='flex flex-row lg:flex-col items-center justify-between lg:justify-start mt-4 lg:mt-0'>
+                                        <button className='bg-white rounded lg:rounded-full text-3xl p-3 flex items-center justify-center'>
+                                            <FaEdit />
+                                            <span className='ms-4 block lg:hidden'>Edit Book</span>
+                                        </button>
+                                        <button className='text-red-500 rounded lg:rounded-full text-3xl p-3 mt-0 lg:mt-8 bg-white flex items-center justify-center'>
+                                            <MdDeleteOutline />
+                                            <span className='ms-4 block lg:hidden'>Delete Book</span>
+                                        </button>
+                                    </div>
+                                )
+                            }
+                        </div>
                     </div>
+
                     <div className='p-4 w-full lg:w-3/6'>
                         <h1 className='text-4xl text-zinc-300 font-semibold'>{Data.title}</h1>
                         <p className='text-zinc-400 mt-1'>by {Data.author}</p>
