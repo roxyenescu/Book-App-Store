d7775e9d26c7a504498de3211127a938a86afc2d
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2025-05-14 21:53:41 +0300

    Frontend: Add to Cart feature

--- client/src/pages/AllBooks.jsx
+++ client/src/pages/AllBooks.jsx
@@ -19,21 +19,17 @@ const AllBooks = () => {
     <div className='bg-zinc-900 h-auto px-12 py-8'>
       {" "}
       <h4 className='text-3xl text-yellow-100'>All books</h4>
-      {
-        !Data && (
-          <div className='flex items-center justify-center my-8'>
-            <Loader />{" "}
-          </div>
-        )
-      }
+      {!Data && (
+        <div className='w-full h-screen flex items-center justify-center'>
+          <Loader />
+        </div>
+      )}
       <div className='my-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-8'>
-        {
-          Data && Data.map((items, i) => (
-            <div key={i}>
-              <BookCard data={items} />{" "}
-            </div>
-          ))
-        }
+        {Data && Data.map((items, i) => (
+          <div key={i}>
+            <BookCard data={items} />{" "}
+          </div>
+        ))}
       </div>
     </div>
   )
