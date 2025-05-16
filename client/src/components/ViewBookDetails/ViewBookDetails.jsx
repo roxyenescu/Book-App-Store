ed580f6a912eaaed29c9256c8f36116ce9d6d8b5
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2025-05-16 22:52:54 +0300

    Frontend: Admin Panel - Delete a book functionality

--- client/src/components/ViewBookDetails/ViewBookDetails.jsx
+++ client/src/components/ViewBookDetails/ViewBookDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Loader from '../Loader/Loader';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { GrLanguage } from "react-icons/gr";
 import { FaHeart } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
@@ -11,6 +11,7 @@ import { MdDeleteOutline } from "react-icons/md";
 
 const ViewBookDetails = () => {
     const { id } = useParams();
+    const navigate = useNavigate();
     const [Data, setData] = useState();
     const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
     const role = useSelector((state) => state.auth.role);
@@ -39,12 +40,21 @@ const ViewBookDetails = () => {
         alert(response.data.message);
     };
 
-    const handlerCart = async() => {
-        const response = await axios.put("http://localhost:1000/api/v1/add-to-cart", 
-            {}, 
+    const handlerCart = async () => {
+        const response = await axios.put("http://localhost:1000/api/v1/add-to-cart",
+            {},
             { headers }
         );
         alert(response.data.message);
+    };
+
+    const deleteBook = async () => {
+        const response = await axios.delete(
+            "http://localhost:1000/api/v1/delete-book",
+            { headers }
+        );
+        alert(response.data.message);
+        navigate("/all-books");
     };
 
     return (
@@ -54,41 +64,40 @@ const ViewBookDetails = () => {
                     <div className='w-full lg:w-3/6'>
                         <div className='flex flex-col lg:flex-row justify-around bg-zinc-800 p-12 rounded'>
                             <img src={Data.url} alt='/' className='h-[50vh] md:h-[60vh] lg:h-[70vh] rounded' />
-                            {
-                                isLoggedIn === true && role === "user" && (
-                                    <div className='flex flex-col md:flex-row lg:flex-col items-center justify-between lg:justify-start mt-4 lg:mt-0'>
-                                        <button
-                                            className='bg-white rounded lg:rounded-full text-4xl lg:text-3xl p-3 text-red-500 flex items-center justify-center'
-                                            onClick={handlerFavourite}
-                                        >
-                                            <FaHeart />
-                                            <span className='ms-4 block lg:hidden'>Add to Favourites</span>
-                                        </button>
-                                        <button
-                                            className='text-white rounded mt-8 md:mt-0 lg:rounded-full text-4xl lg:text-3xl p-3 lg:mt-8 bg-blue-500 flex items-center justify-center'
-                                            onClick={handlerCart}
-                                        >
-                                            <FaShoppingCart />
-                                            <span className='ms-4 block lg:hidden'>Add to Cart</span>
-                                        </button>
-                                    </div>
-                                )
-                            }
+                            {isLoggedIn === true && role === "user" && (
+                                <div className='flex flex-col md:flex-row lg:flex-col items-center justify-between lg:justify-start mt-4 lg:mt-0'>
+                                    <button
+                                        className='bg-white rounded lg:rounded-full text-4xl lg:text-3xl p-3 text-red-500 flex items-center justify-center'
+                                        onClick={handlerFavourite}
+                                    >
+                                        <FaHeart />
+                                        <span className='ms-4 block lg:hidden'>Add to Favourites</span>
+                                    </button>
+                                    <button
+                                        className='text-white rounded mt-8 md:mt-0 lg:rounded-full text-4xl lg:text-3xl p-3 lg:mt-8 bg-blue-500 flex items-center justify-center'
+                                        onClick={handlerCart}
+                                    >
+                                        <FaShoppingCart />
+                                        <span className='ms-4 block lg:hidden'>Add to Cart</span>
+                                    </button>
+                                </div>
+                            )}
 
-                            {
-                                isLoggedIn === true && role === "admin" && (
-                                    <div className='flex flex-col md:flex-row lg:flex-col items-center justify-between lg:justify-start mt-4 lg:mt-0'>
-                                        <button className='bg-white rounded lg:rounded-full text-4xl lg:text-3xl p-3 flex items-center justify-center'>
-                                            <FaEdit />
-                                            <span className='ms-4 block lg:hidden'>Edit Book</span>
-                                        </button>
-                                        <button className='text-red-500 rounded lg:rounded-full text-4xl lg:text-3xl p-3 mt-8 md:mt-0 lg:mt-8 bg-white flex items-center justify-center'>
-                                            <MdDeleteOutline />
-                                            <span className='ms-4 block lg:hidden'>Delete Book</span>
-                                        </button>
-                                    </div>
-                                )
-                            }
+                            {isLoggedIn === true && role === "admin" && (
+                                <div className='flex flex-col md:flex-row lg:flex-col items-center justify-between lg:justify-start mt-4 lg:mt-0'>
+                                    <button className='bg-white rounded lg:rounded-full text-4xl lg:text-3xl p-3 flex items-center justify-center'>
+                                        <FaEdit />
+                                        <span className='ms-4 block lg:hidden'>Edit Book</span>
+                                    </button>
+                                    <button
+                                        className='text-red-500 rounded lg:rounded-full text-4xl lg:text-3xl p-3 mt-8 md:mt-0 lg:mt-8 bg-white flex items-center justify-center'
+                                        onClick={deleteBook}
+                                    >
+                                        <MdDeleteOutline />
+                                        <span className='ms-4 block lg:hidden'>Delete Book</span>
+                                    </button>
+                                </div>
+                            )}
                         </div>
                     </div>
 
