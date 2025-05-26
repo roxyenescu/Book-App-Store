b8c3584b2b93ae5ed4e7d055198cf0ef0f10b24f
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2025-05-26 22:33:00 +0300

    Frontend: Implement OriginalPicks responsive component and integrate into Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Home/Hero'
 import RecentlyAdded from '../components/Home/RecentlyAdded';
 import HotThisWeek from '../components/Home/HotThisWeek';
 import MostPurchasedThisWeek from '../components/Home/MostPurchasedThisWeek';
+import OriginalPicks from '../components/Home/OriginalPicks';
 import RecommendedBoth from '../components/Home/RecommendedBoth';
 import RecommendedByGenre from '../components/Home/RecommendedByGenre';
 import RecommendedByAuthor from '../components/Home/RecommendedByAuthor';
@@ -15,6 +16,7 @@ const Home = () => {
       <RecentlyAdded />
       <HotThisWeek />
       <MostPurchasedThisWeek />
+      <OriginalPicks />
       <RecommendedBoth />
       <RecommendedByGenre />
       <RecommendedByAuthor />
